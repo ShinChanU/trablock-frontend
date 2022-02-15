a2cd7e3dee5c52cf782e063f82253fb6bafdad3a
ShinChanU <tlscksdn963@naver.com>
2022-02-15 19:01:15 +0900

    dnd testing, multiple list test

--- src/test/DndTestPage.js
+++ src/test/DndTestPage.js
@@ -25,8 +25,48 @@ const Div = styled.div`
   margin: 30px;
 `;
 
+const getItems = (count, offset = 0) => {
+  return Array.from({length: count}, (v, k) => k).map(k => ({
+    id: `item-${k + offset}`,
+    content: `item ${k + offset}`
+  }));
+};
+
+// dnd 후 배열 재정렬 로직
+const reorder = (data, startIndex, endIndex) => {
+  const items = [...data];
+  const [removed] = items.splice(startIndex, 1); // splice는 제거한 요소를 담은 배열 리턴
+  items.splice(endIndex, 0, removed); // 기존 배열에서 움직인 요소를 목적지에 저장
+
+  return items;
+};
+
 const DndTestPage = () => {
   const [data, setData] = useState([]);
+
+  // const [data, setData] = useState({
+  //   items: getItems(10),
+  //   selected: getItems(5, 10)
+  // });
+
+  // const {items, selected} = data;
+  // // const [items, setItems] = useState([]);
+  // // const [selected, setSelected] = useState([]);
+
+  // // list마다 고유 id
+  // const idList = {
+  //   droppable: 'items',
+  //   droppable2: 'selected'
+  // };
+
+  // const getList = (id) => {
+  //   // return 
+  //   setData({
+  //     ...setData,
+  //     idList(id) 
+  //   })
+  // }
+
   useEffect(() => {
     axios.get('http://localhost:4000/travelPlans/1')
     .then(({ data }) => {
@@ -37,44 +77,54 @@ const DndTestPage = () => {
     });
   }, []);
   
-  console.log(data);
-  // 드래그 끝났을 시
-  const onDragEnd = (result) => { 
-    if(!result.destination) return ;
-    console.log('result ?', result);
-    const items = [...data];
-    // console.log(items);
-    console.log(result.source.index);
+  // 드래그 끝났을 시, 배열 상태 저장 메서드(메모리상에서 되는 것 같음)
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+    if(!destination) return ;
 
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    // console.log(reorderedItem);
+    // 같은 리스트에서 dnd
+    if(source.droppableId === destination.droppableId) {
+      const items = reorder(
+        getList(source.droppableId),
+
+      )
+    }
+    // const items = [...data]; // 움직이지 않은 요소들만..
+    // console.log(items);
+    // const [reorderedItem] = items.splice(source.index, 1); // splice는 제거한 요소를 담은 배열 리턴
+    // items.splice(result.destination.index, 0, reorderedItem); // 기존 배열에서 움직인 요소를 목적지에 저장
+    
+    setData(items);
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext 
+      onDragEnd={onDragEnd}
+    >
       <h1>DragDropContext</h1>
       <Body>
       <BigDiv>
       <h2>droppable</h2>
-        <Droppable droppableId={String('test')}>
+        <Droppable droppableId="droppable">
         {provided => (
           <div 
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-          // style={style.container}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
           >
             {data.map(({id, name}, index) => (
-              <Draggable draggableId={String(id)} index={index}>
-                {provided => (
-                  <div
+              <Draggable 
+                key={id} 
+                draggableId={String(id)} 
+                index={index}
+              >
+                {provided => (    
+                  <Div 
+                    ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                  >      
-                    <Div key={id} name={name} index={index} id={id} >
-                      {name}
-                    </Div>
-                  </div>
+                  >
+                    {name}
+                  </Div>
                 )}
               </Draggable>
             ))}
@@ -83,27 +133,28 @@ const DndTestPage = () => {
         )}
         </Droppable>
       </BigDiv>
-      {/* <BigDiv>
+      <BigDiv>
       <h2>droppable</h2>
-        <Droppable droppableId="test2">
+        <Droppable droppableId="droppable2">
         {provided => (
           <div 
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-          // style={style.container}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
           >
             {data.map(({id, name}, index) => (
-              <Draggable draggableId={String(id)} index={index}>
-                {provided => (
-                  <div
+              <Draggable 
+                key={id} 
+                draggableId={String(id+"1")} 
+                index={index}
+              >
+                {provided => (    
+                  <Div 
+                    ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                  >      
-                    <Div key={id} name={name} index={index} id={id} >
-                      {name}
-                    </Div>
-                  </div>
+                  >
+                    {name}
+                  </Div>
                 )}
               </Draggable>
             ))}
@@ -111,7 +162,7 @@ const DndTestPage = () => {
           </div>
         )}
         </Droppable>
-      </BigDiv> */}
+      </BigDiv>
       </Body>
     </DragDropContext>
   );
